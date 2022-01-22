79526c0c23578c007a188bac72169314cb8a75cf
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-01-22 16:43:23 -0500

    Week 2

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Campsiteinfo extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         selectedCampsite: null
+    //     };
+    // }
+
+    renderCampsite(campsite) {
+        return (
+          <div className="col-md-5 m-1">
+           <Card>
+           <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                <CardTitle>{campsite.name}</CardTitle>
+                <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>    
+            </div>        
+        )
+    }
+    renderComments(comments){
+        if (comments){
+            return (
+                <div className ="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {
+                        comments.map(comment => {
+                            return (
+                            <p key={comment.id}>{comment.text}<br/>
+                            --{comment.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </p>)
+                        })
+                    }
+                    
+                </div>
+            )
+            
+        }
+    }
+
+render (){
+    if(this.props.campsite){
+        return (
+            <div className="row">{this.renderCampsite(this.props.campsite)}
+            {this.renderComments(this.props.campsite.comments)}
+            </div>
+        )
+    } else {
+        return <div></div>
+    }
+}
+
+}
+
+
+    export default Campsiteinfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
+
 
 class Directory extends Component {
     constructor(props) {
@@ -11,24 +14,24 @@ class Directory extends Component {
         this.setState({selectedCampsite: campsite});
     }
     
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
-    }
+    // renderSelectedCampsite(campsite) {
+    //     if (campsite) {
+    //         return (
+    //             <Card>
+    //                 <CardImg top src={campsite.image} alt={campsite.name} />
+    //                 <CardBody>
+    //                     <CardTitle>{campsite.name}</CardTitle>
+    //                     <CardText>{campsite.description}</CardText>
+    //                 </CardBody>
+    //             </Card>
+    //         );
+    //     }
+    //     return <div />;
+    // }
     render() {
         const directory = this.props.campsites.map(campsite => {
             return (
-                <div key={campsite.id} className="col-md-5 m-1">
+                    <div key={campsite.id} className="col-md-5 m-1">
                     <Card onClick={() => this.onCampsiteSelect(campsite)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
@@ -44,11 +47,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+              <CampsiteInfo campsite = {this.state.selectedCampsite}/>
             </div>
         );
     }
