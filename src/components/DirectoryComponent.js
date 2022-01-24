1f67e70c2842ee65f7b70c79f802880328572208
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-01-24 16:43:33 -0500

    'components'

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -4,21 +4,12 @@ import CampsiteInfo from './CampsiteInfoComponent';
 
 
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
 
     render() {
         const directory = this.props.campsites.map(campsite => {
             return (
                     <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                    <Card onClick={()=>this.props.onClick(campsite.id)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -33,7 +24,6 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-              <CampsiteInfo campsite = {this.state.selectedCampsite}/>
             </div>
         );
     }
