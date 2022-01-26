a8006983fcedcdc6c66d2e67f0722183de11602c
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-01-26 16:57:26 -0500

    'Breadcrumbs'

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDirectoryItem({campsite}) {
     return (
         <Card>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+        <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+        </Link>
         </Card>
     );
 }
@@ -22,13 +25,22 @@ function Directory(props) {
         );
     });
 
-    return (
+return (
         <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
             <div className="row">
                 {directory}
             </div>
         </div>
     );
 }
-
 export default Directory;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+
+
 class Main extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -18,9 +28,25 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         };
+
+
+        const CampsiteWithId=({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };    
+
+        <Route path='/directory/:campsiteId' component={CampsiteWithId} />
 
         return (
             <div>
@@ -28,7 +54,10 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId'component={CampsiteWithId}/>
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
+        
                 </Switch>
                 <Footer />
             </div>
