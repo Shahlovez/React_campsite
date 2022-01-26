3524b5d1b772beb017900d15b5ccaa8c4f87c02e
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-01-26 16:57:07 -0500

    'Breadcrumbs'

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText,CardBody,CardTitle, Breadcrumb,BreadcrumbItem,} from "reactstrap";
+import { Link } from 'react-router-dom';
 
 
     function RenderCampsite({campsite}) {
@@ -8,7 +9,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
            <Card>
            <CardImg top src={campsite.image} alt={campsite.name} />
                 <CardBody>
-                <CardTitle>{campsite.name}</CardTitle>
+                {/* <CardTitle>{campsite.name}</CardTitle> */}
                 <CardText>{campsite.description}</CardText>
                 </CardBody>
             </Card>    
@@ -25,6 +26,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                             <p key={comment.id}>{comment.text}<br/>
                             --{comment.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                             </p>)
+                           
                         })
                     }
                     
@@ -40,10 +42,10 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                 <div className="container">
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.campsite.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
-            );
+        );
         }
         return <div />;
     }
