5bb173ec515e1b9e142564b8261453c138ff7b49
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-01-29 00:01:45 -0500

    'Week 3'

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -56,6 +57,8 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId'component={CampsiteWithId}/>
                     <Route exact path='/contactus' component={Contact} />
+                    {/* Task 1 */}
+                    <Route exact path ='/aboutus' render={()=><About partners={this.state.partners}/>}/>
                     <Redirect to='/home' />
         
                 </Switch>
