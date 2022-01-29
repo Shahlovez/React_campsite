c78d82b12185c206c1888e6280e7aa684a6b1e30
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-01-29 00:04:51 -0500

    'Week 3'

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -9,7 +9,9 @@ function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner={partner} />
+            </Media>
         );
     });
 
