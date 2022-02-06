3d1f7b8c7609a066106ac9b79f2d7686a9f806e5
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-02-05 23:34:35 -0500

    Comment Form

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,20 +1,105 @@
 import React from 'react';
-import { Card, CardImg, CardText,CardBody, CardTitle, Breadcrumb,BreadcrumbItem,} from "reactstrap";
+import { Card, CardImg, CardText,CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label} from "reactstrap";
 import { Link } from 'react-router-dom';
+// import ModalHeader from 'reactstrap/lib/ModalHeader';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 
-    function RenderCampsite({campsite}) {
-        return (
-          <div className="col-md-5 m-1">
-           <Card>
-           <CardImg top src={campsite.image} alt={campsite.name} />
-                <CardBody>
-                <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card>    
-            </div>        
-        )
-    }
+    
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+function RenderCampsite({campsite}) {
+  return (
+    <div className="col-md-5 m-1">
+     <Card>
+     <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+          <CardText>{campsite.description}</CardText>
+          </CardBody>
+      </Card>    
+      </div>        
+  )
+}
+class CommentForm extends React.Component{
+        constructor(props) {
+          super(props);
+          this.state = {
+              isModalOpen: false
+          }
+
+          this.toggleModal = this.toggleModal.bind(this);
+      }
+
+      toggleModal() {
+          this.setState({
+              isModalOpen: !this.state.isModalOpen
+          });
+      };
+      handleSubmit = (values) => {
+        this.toggleModal();
+        console.log(values);
+      };
+
+  render(){
+            return(
+              <div>
+                <Button outline onClick={this.toggleModal}>
+                  <i className="fa fa-pencil fa-lg" /> Submit Comment</Button>
+                
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                    
+                    <div className='form-group'>
+                        <Label htmlFor='rating'>Rating</Label>
+                        <Control.select model='.rating' id='rating' name='rating' className='form-control'>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Control.select>
+                    </div>
+
+                    <div className='form-group'>
+                    <Label htmlFor='author'>Your Name</Label>
+                    <Control.text model='.author' id='author' name='author' placeholder='Your Name' className='form-control'
+                        validators={{
+                            minLength:minLength(2), 
+                            maxLength:minLength(15)
+                            }}
+                            />
+                    <Errors
+                    className='text-danger'
+                    model='.author'
+                    show='touched'
+                    component='div'
+                    messages={{
+                            minLength: 'Must be at least 2 characters', 
+                            maxLength:'Must be 15 characters or less'
+                            }}
+                            />
+                    </div>
+
+                    <div className='form-group'>
+                    <Label htmlFor='text' md={12}>Comment</Label>
+                    <Control.textarea model='.text' id='text' name='text' rows='6' className='form-control'/>
+                    </div>
+                 
+                    <Button type='submit' color='primary'>Submit</Button>
+              
+                    </LocalForm>
+                </ModalBody>
+
+                </Modal>
+                </div>
+            );
+        }
+
+}
+
     function RenderComments({comments}){
         if (comments){
             return (
@@ -34,6 +119,7 @@ import { Link } from 'react-router-dom';
                     </div>
           );
         })}
+        <CommentForm />
       </div>
     );
   }
